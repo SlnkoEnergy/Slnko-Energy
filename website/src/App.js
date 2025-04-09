61b25c5c3afbc8818ad3b92426100fb834220bec
KumarRounik <rounikpandey@gmail.com>
2025-04-09 17:14:16 +0530

    Page 9 added

--- website/src/App.js
+++ website/src/App.js
@@ -11,11 +11,11 @@ import Page4 from "./Pages/Page4"; // Import your Page4 component
 import Page5 from "./Pages/Page5"; // Import your Page5 component
 import Page6 from "./Pages/Page6";
 import Page7 from "./Pages/Page7";
+import Page9 from "./Pages/Page9";
 
 // import Page7N from "./Pages/Page7.0";
 
-import Page4 from "./Pages/Page4"; // Import your Page4 component
-import Page5 from "./Pages/Page5"; // Import your Page5 component
+
 
 
 const App = () => {
@@ -26,18 +26,18 @@ const App = () => {
 
         <Page2 />
         <Page3 />
-        {/* <Page4/>   */}
+        <Page4/>  
         <Page5/>
         <Page6/>
         <Page7/>       
-       
+       <Page9/>
         <Footer />
 
         {/* <Page2 /> */}
 
         
 
-        <Page4/>
+        {/* <Page4/> */}
 
         {/* <Footer /> */}
 
