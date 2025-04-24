0d5aeebc43a04ca51224b8e65412665617832130
KumarRounik <rounikpandey@gmail.com>
2025-04-24 14:47:16 +0530

    About refined

--- website/src/index.js
+++ website/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "../src/CSS/Index.css"; // Importing global CSS styles
+import "../src/CSS/Index.css"
 import "@fontsource/poppins"; // Importing Poppins font
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
