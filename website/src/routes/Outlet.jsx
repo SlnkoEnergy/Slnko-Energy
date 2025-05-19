6ce973c56efb2a67f4e28acd30a185eb4a915ba2
Siddharth Singh <singhsiddharth1102@gmail.com>
2025-05-19 12:08:40 +0530

    Added Routing of Services

---
+++ website/src/routes/Outlet.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <div>
+      {/* Optional: Add a sidebar or heading for services here */}
+      <Outlet />
+    </div>
+  );
+};
+
+export default Layout;
